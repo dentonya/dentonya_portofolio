2937f813017289d6fb2cc23050141c8f67f81141
Denatonya <denatonya@gmail.com>
2023-07-03 16:39:36 +0300

    update on image and navbar

--- portofolio/src/components/Home.jsx
+++ portofolio/src/components/Home.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import HeroImage from '../assets/heroImage.png';
-import {FiArrowRightCircle} from 'react-icons/fi';
-import { Link } from 'react-scroll';
-
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { FiArrowRightCircle } from "react-icons/fi";
 
 export const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'> 
-        <div className='max-w-screen-lg mx-auto flex flex-col items justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'> Hey I'm Atonya Dennis</h2>
-                <p className='text-gray-500 py-4 mx-w-md'>
-                A software developer focused building the Frontend and
-                 Backend of Websites and Web Applications that leads to the success of the overall product
-                </p>
-                <div>
-
-                  <a
-  href={process.env.PUBLIC_URL + '/Atonya_Dennis_Resume.pdf'}
-  className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'
-  download
->
-  Resume/Cv
-  <span className='group-hover:rotate-90 duration-300'>
-    <FiArrowRightCircle size={20} className='ml-1' />
-  </span>
-</a>
-
-                </div>
-
-                
-            </div>
-            <div>
-            <img
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center md:mr-8">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white mb-4 md:mb-8">
+            Hey, I'm Atonya Dennis
+          </h2>
+          <p className="text-gray-500 max-w-md text-center md:text-left">
+            A software developer focused on building the Frontend and Backend of
+            Websites and Web Applications that lead to the success of the
+            overall product.
+          </p>
+          <div className="mt-4">
+            <a
+              href={process.env.PUBLIC_URL + "/Atonya_Dennis_Resume.pdf"}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+              download
+            >
+              Resume/CV
+              <span className="group-hover:rotate-90 duration-300">
+                <FiArrowRightCircle size={20} className="ml-1" />
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-16 my-7 w-2/3 md:w-full md:my-20 md:ml-8"
+            className="rounded-2xl mx-auto my-7 w-full"
           />
-                {/* <img src={HeroImage} alt="My Profile" className='rounded-2xl mx-auto w-3/4 md:w-full hidden md:block' /> */}
-            </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- portofolio/src/components/NavBar.jsx
+++ portofolio/src/components/NavBar.jsx
@@ -1,68 +1,92 @@
-import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
-import {Link} from 'react-scroll' // npm install react-scroll
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import HeroImage from "../assets/heroImage.png";
+import { Link } from "react-scroll"; // npm install react-scroll
 
 export const NavBar = () => {
-    // state for mobile navigation menu
-    const [nav, setNav] = useState(false);
+  // state for mobile navigation menu
+  const [nav, setNav] = useState(false);
 
-    // array of links
-    const links = [
-        {
-            id: 1,
-            link: 'home',
-        },
-        {
-            id: 2,
-            link: 'about',
-        },
-        {
-            id: 3,
-            link: 'portfolio',
-        },
-        {
-            id: 4,
-            link: 'experience',
-        },
-        {
-            id: 5,
-            link: 'contact',
-        },
-    ];
+  // array of links
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
-        <div>
-            <h1 className='text-5xl font-signature ml-2'>Dennis</h1>
-        </div>
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <div className="flex items-center">
+        <img
+          src={HeroImage}
+          alt="my profile"
+          className="rounded-full h-auto w-12"
+        />
+        <h1 className="text-2xl font-signature ml-4">ATONYA DENNIS</h1>
+      </div>
 
-        {/* Navigation Links */}
-        <ul className='hidden md:flex'> {/* hidden on mobile*/}
-            {links.map(({id,link}) => (
-                <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
-                    <Link to={link} smooth duration={500}>{link}</Link>
-                </li>
-            ))}
+      {/* Navigation Links */}
+      <ul className="hidden md:flex">
+        {" "}
+        {/* hidden on mobile*/}
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      {/* implement mobile navigation menu */}
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {/* close the mobile navigation menu when clicked outside */}
+      {nav && (
+        <ul
+          className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800
+         text-gray-500"
+        >
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link
+                onClick={() => setNav(!nav)}
+                to={link}
+                smooth
+                duration={500}
+              >
+                {link}
+              </Link>
+            </li>
+          ))}
         </ul>
-        {/* implement mobile navigation menu */}
-        <div onClick={() => setNav(!nav)}   className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
-          {
-                nav ? <FaTimes size={30} /> : <FaBars size={30}/>
-          }
-        </div>
-
-        {/* close the mobile navigation menu when clicked outside */}
-        {nav && (     
-        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800
-         text-gray-500'>
-            {links.map(({id,link}) => (
-                <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>
-                    <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
-                </li>
-            ))}
-
-        </ul>
-        )}
-       
+      )}
     </div>
-  )
-}
+  );
+};
